673536b945070a422745595a692ed0b2bb11f335
leander@kafemann.berlin <leander@kafemann.berlin>
2025-05-26 16:19:42 +0200

    add cookie set site

---
+++ js/cookieSet.js
@@ -0,0 +1,28 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function getVerifyCodeFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('verify-code');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const code = getVerifyCodeFromUrl();
+    const messageDiv = document.getElementById('status-message');
+    if (code) {
+        setCookie('verify-code', code, 31);
+        messageDiv.textContent = "Verifizierungscode gespeichert. Weiterleitung...";
+        setTimeout(() => {
+            window.location.href = "../";
+        }, 1200);
+    } else {
+        messageDiv.textContent = "Kein Verifizierungscode in der URL gefunden.";
+    }
+});
